2356ed99429210c1c2c1c318d5c805887346be74
krish0raj0vaiskiyar <71975056+krish0raj0vaiskiyar@users.noreply.github.com>
2020-11-03 10:13:54 +0530

    Add files via upload

---
+++ Solved answer of Assignrnt 3/find leap year by krish raj.py
@@ -0,0 +1,9 @@
+print ("This program is made by Krish Raj")
+print ("this program is made for find leap year")
+a = int (input("enter the year :  "))
+b = a%4
+if b==0 :
+    print ("this is a leap year")
+if b!= 0 :
+    print ("this is not a leap year")
+print ("thank you for using this program")
